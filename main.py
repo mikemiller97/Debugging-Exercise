717fd359ea83f76b80408188feb77a2b1a5cc0e4
Mike Miller <>
2023-02-07 19:15:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+def myFunction(numList, num):
+           counter = 0
+           for elem in range(numList):
+               if elem < num
+                   counter += 1
+       # generate a list of 10 random integer.
+       for i in range(10):
+           list1.append(randint(0, 10))
+       myFunction(list1)
+       print(counter)
